c5a91f0a896e14026110f6e5e51414d1e57987d0
Nicoogf <nicolasgfalabella@gmail.com>
2023-09-25 11:40:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect , useState } from 'react';
+import './App.css' ;
+
+function App() {
+  
+  const [ users , setUsers ] = useState( [] )
+
+useEffect( () =>{
+  fetch( "https://randomuser.me/api?results=100")
+  .then( res => res.json())
+  .then( res => 
+    setUsers ( res.results )
+  )
+  .catch( err => {
+    console.log(err)
+  })  
+})
+
+
+
+
+
+  return (
+    <div className='App'>
+      <h1>Prueba Tecnica.</h1>
+    </div>
+  )
+}
+
+export default App
