f188bb973cc18466b983dc894022a64237747df9
Nicoogf <nicolasgfalabella@gmail.com>
2023-09-27 08:21:17 -0300

    Se inicia nuevamente el proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,135 +1,54 @@
-import { useEffect , useMemo, useRef, useState } from 'react';
+import { useEffect , useState} from "react" ;
 import './App.css' ;
-import { SortBy, type User } from "./types.d" ; 
-import { UserList } from './Components/UserList';
+
+import { type User } from "./types.d"; 
+import { UserList } from "./Components/UserList";
 
 function App() {
-  
-  const [ users , setUsers ] = useState<User[]>([]) ;
-  const [ showColors , setShowColors] = useState( false );
-  const [ sorting , setSorting ] = useState<SortBy>( SortBy.NONE ) ;
-  const [ filterCountry , setFilterCountry ] = useState<string|null> ( null )
 
-  const [isLoading , setIsLoading ] = useState( false );
-  const [ error , setError ] = useState ( false );
-
-  const [ currentPage , setCurrentPage ] = useState( 1 )
- 
-  const EstadoOriginal = useRef<User[]>([]) ;
-
-    const toggleColors = () =>{
-      setShowColors( !showColors )
-    }
-
-    const toggleSortByCountry = () =>{
-      const newSortingValue = sorting === SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE
-      setSorting( newSortingValue )
-    } 
-
-    const eliminarUsuario = ( email: string ) =>{
-      const usuariosFiltrados = users.filter(( usuario )=> usuario.email !==  email)
-      setUsers(usuariosFiltrados)
-    }
-
-    const handleReset = () =>{
-      setUsers( EstadoOriginal.current )
-    }
-
-    const handleChangeSort = (sort:SortBy) =>{
-      setSorting(sort)
-    } 
-
-    useEffect(() => {
-      setIsLoading(true);
-      setError(false);
-
-      fetch(`https://randomuser.me/api?results=10&seed=practica&page=${ currentPage}`)
-        .then((res) => {
-          if(!res.ok) throw new Error ("Error en la peticion")
-          return res.json()
-        })      
-        .then((res) => {
-          setUsers( prevUsers => prevUsers.concat(res.results));
-          EstadoOriginal.current = res.results;
-        })
-        .catch((err) => {
-          setError(err); 
-          console.log(err);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    }, [currentPage]);
+/**
+ * Estado de usuarios : 
+ *  users = usuarios
+ *  setUsers = setUsuarios
+ *  El estado inicial va a ser un Array Vacio de tipo User
+ */
+  const [ usuario , setusuario ] = useState<Array<User[]>>([])
 
 
-   
-      const filteredUsers = useMemo( () => {
-        console.log ( "Calculando FilteredUsers")
-        return filterCountry != null && filterCountry.length > 0
-          ? users.filter ( user => {
-            return user.location.country.toLocaleLowerCase().includes(filterCountry.toLocaleLowerCase())
-          })
-          : users
-     } ,[ users , filterCountry])
-
-     
-
-     const sortedUsers = useMemo(() => {
-      console.log('calculate sortedUsers')
-  
-      if (sorting === SortBy.NONE) return filteredUsers
-  
-      const compareProperties: Record<string, (user: User) => any> = {
-        [SortBy.COUNTRY]: user => user.location.country,
-        [SortBy.NAME]: user => user.name.first,
-        [SortBy.LAST]: user => user.name.last
-      }
-  
-      return filteredUsers.toSorted((a, b) => {
-        const extractProperty = compareProperties[sorting]
-        return extractProperty(a).localeCompare(extractProperty(b))
-      })
-    }, [filteredUsers, sorting])
 
 
-  return (
-    <div className='App'>
+  /**
+   *  Se usa UseEffect para hacer el fetch a la api
+   *  res = respuesta 
+   *  respuesta.result = Accede al resultado del llamado a la Api a la propiedad Results
+   *  (Que contiene el listado de usuarios)
+   *  
+  */
 
-      <h1>Prueba Tecnica.</h1>
+  useEffect( () => {
+   fetch( 'https://randomuser.me/api/?page=3&results=100' )
+    .then( respuesta => respuesta.json())
+    .then( respuesta => {
+      setusuario( respuesta.results )
+    })
+    .catch( err => {
+      console.error( err )
+    })
+    .finally()
+  } , [] )
 
-      <header>
-        <button onClick={ toggleColors }> Colorear Filas </button>
 
-        <button onClick={ toggleSortByCountry }> 
-          { sorting === SortBy.COUNTRY ? "No ordenar Paises" : "Ordenar Paises"}  
-        </button>
 
-        <button onClick={ handleReset}>
-          Resetear estado
-        </button>
 
-        <input placeholder='Filtrar por pais' onChange={(e)=>setFilterCountry(e.target.value)}></input>
-      </header>
+  return(
+    <div>
 
-      <main>
+      <h1>Listado de Usuarios</h1>
 
-      { users.length > 0 &&    <UserList changeSorting = { handleChangeSort } users={ sortedUsers }  showColors = { showColors }  eliminarUsuario = {eliminarUsuario}/> }
+       <UserList />
 
-      { isLoading && <p> Cargando Datos ...</p>} 
-
-      { !isLoading && error && <p> Ocurrio un Problema intente mas tarde </p>}
-
-      { !isLoading && !error && users.length === 0 && <p>No hay Usuarios para mostrar </p>}
-
-        
-
-      { !isLoading && !error && <button onClick={ () => setCurrentPage (currentPage+1) }> Cargar mas Usuarios </button> }
-     
-      </main>
-
-    
     </div>
-  )
-}
-
-export default App
+  ) ;
+} ;
+ 
+export default App ;
--- src/Components/UserList.tsx
+++ src/Components/UserList.tsx
@@ -1,46 +1,7 @@
-import { SortBy, type User } from "../types.d" ; 
-
-interface Props{
-    users: User[]
-    showColors: boolean
-    eliminarUsuario : ( email:string) => void
-    changeSorting : ( sort:SortBy) => void 
-}
-
-
-export function UserList ({ changeSorting , eliminarUsuario , users , showColors }: Props) {
+export function UserList () {
     return(
         <table width='100%'>
-            <thead>
-                <th>Foto</th>
-                <th className='pointer' onClick={()=> changeSorting(SortBy.NAME)}>Nombre</th>
-                <th className='pointer' onClick={()=> changeSorting(SortBy.LAST)}>Apellido</th>
-                <th className='pointer' onClick={()=> changeSorting(SortBy.COUNTRY)}>Pais</th>
-                <th>Accion</th>
-            </thead>
-
-
-            <tbody>
-                 
-                {
-                    users.map( (user , index )=>{
-                    const backgroundColor =  index  % 2 === 0 ?  "#333" : "#555 "
-                    const color = showColors ?  backgroundColor : "transparent"
-                        return (
-                        <tr key={ user.email } style={ {backgroundColor: color } }> 
-                            <td>
-                                <img src ={ user.picture.thumbnail }></img>
-                            </td>
-
-                            <td> { user.name.first } </td>
-                            <td> { user.name.last } </td>
-                            <td> { user.location.country } </td>
-                            <td> <button onClick={ () => eliminarUsuario(user.email)}> Eliminar </button> </td>
-                        </tr>
-                        )
-                    })
-                }
-            </tbody>
+           
         </table>
     )
 }
